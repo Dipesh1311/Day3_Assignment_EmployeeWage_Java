5e76916b0df05c5c8d715c0bb470f7fc090f1ac7
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-24 18:37:10 +0530

    Employee daily wage program

--- src/com/day3/employee_wage/employeeWage.java
+++ src/com/day3/employee_wage/employeeWage.java
@@ -1,8 +1,13 @@
 package com.day3.employee_wage;
 
+import java.time.DayOfWeek;
 import java.util.Random;
 
 public class employeeWage {
+
+    static final int EMP_WAGE_PER_HOUR = 20;
+    static final int EMP_FULL_DAY_HOUR = 8;
+    static int DailyWage;
 
     public static void main(String[] args) {
 
@@ -11,7 +16,9 @@ public class employeeWage {
        System.out.println(EmpAttendance);
 
        if (EmpAttendance == 1) {
-           System.out.println("Employee is presend");
+           System.out.println("Employee is present");
+           DailyWage = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
+           System.out.println("Employee daily wages :"+DailyWage);
        }
            else {
                System.out.println("Employee is absent");

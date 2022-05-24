a04d4efe263b120106593cdcc028c09f5746bf75
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-24 18:54:26 +0530

    Employee half day wage program

--- src/com/day3/employee_wage/employeeWage.java
+++ src/com/day3/employee_wage/employeeWage.java
@@ -1,18 +1,20 @@
 package com.day3.employee_wage;
 
-import java.time.DayOfWeek;
 import java.util.Random;
 
 public class employeeWage {
 
     static final int EMP_WAGE_PER_HOUR = 20;
     static final int EMP_FULL_DAY_HOUR = 8;
+    static final int EMP_PART_TIME_HOUR = 4;
+
     static int DailyWage;
+    static int HalfDayWage;
 
     public static void main(String[] args) {
 
        Random a = new Random();
-       int EmpAttendance = a.nextInt(2);
+       int EmpAttendance = a.nextInt(3);
        System.out.println(EmpAttendance);
 
        if (EmpAttendance == 1) {
@@ -20,7 +22,12 @@ public class employeeWage {
            DailyWage = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
            System.out.println("Employee daily wages :"+DailyWage);
        }
-           else {
+       else if (EmpAttendance == 2) {
+           System.out.println("Employee is on half day");
+           HalfDayWage = EMP_WAGE_PER_HOUR * EMP_PART_TIME_HOUR;
+           System.out.println("Employee half day wages :"+HalfDayWage);
+       }
+           else  {
                System.out.println("Employee is absent");
        }
     }

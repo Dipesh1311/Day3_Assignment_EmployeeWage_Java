b0c1acba556ebf5274627c78ee6b2b4b5a33599c
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 12:57:57 +0530

    Added implemented program of calculating wage of an employee for a month using condition

--- src/com/day3/employee_wage/employeeWage.java
+++ src/com/day3/employee_wage/employeeWage.java
@@ -1,53 +1,34 @@
 package com.day3.employee_wage;
 
 import java.util.Random;
-
 public class employeeWage {
+        public static void main(String[] args) {
+            int day=1;
+            int full = 8;
+            int half = 4;
+            int hrs = 0;
+            int WagePerHr = 20;
+            Random a = new Random();
 
-    static final int EMP_WAGE_PER_HOUR = 20;
-    static final int EMP_FULL_DAY_HOUR = 8;
-    static final int EMP_HALF_DAY_HOUR = 4;
-    static final int FULL_TIME=1;
-    static final int HALF_TIME=2;
-    static int day;
-    static int totalworkinghrs = 0;
+            while (day<=20 && hrs<=100) {
 
-   // static int DailyWage;
-   // static int HalfDayWage;
+                int EmpAttendance = a.nextInt(3);
+                System.out.println("Day :" +day+ ":"+EmpAttendance);
 
-    public static void main(String[] args) {
-
-       Random a = new Random();
-
-
-       for (day = 1;day<=20;day++) {
-
-           int EmpAttendance = a.nextInt(3);
-            System.out.println("Day :"+ day +":"+ EmpAttendance);
-
-           switch (EmpAttendance) {
-               case FULL_TIME:
-                   totalworkinghrs=totalworkinghrs + EMP_FULL_DAY_HOUR;
-
-                   // System.out.println("Employee is present for full day");
-                   // DailyWage = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
-                   // System.out.println("Employee Daily Wage is : " + DailyWage);
-                    break;
-
-               case HALF_TIME:
-                   totalworkinghrs=totalworkinghrs + EMP_HALF_DAY_HOUR;
-
-                 /*  System.out.println("Employee is Present for half day");
-                   HalfDayWage = EMP_WAGE_PER_HOUR * EMP_HALF_DAY_HOUR;
-                   System.out.println("Employee Daily wage is : " + HalfDayWage); */
-                   break;
-
-               default:
-                   totalworkinghrs=totalworkinghrs + 0;
-                   // System.out.println("Employee is Absent");
-           }
-       }
-       System.out.println("Total working hours of month : "+totalworkinghrs);
-       System.out.println("Total wage of an employee for a month : "+totalworkinghrs*EMP_WAGE_PER_HOUR);
+                if (EmpAttendance == 1 && hrs < 93) {
+                    hrs = hrs + full;
+                    // System.out.println(hrs);
+                }
+                else if (EmpAttendance == 2 && hrs <97) {
+                    hrs = hrs + half;
+                    // System.out.println(hrs);
+                }
+                else {
+                    hrs = hrs + 0;
+                }
+                day++;
+            }
+            System.out.println("total hrs :"+hrs);
+            System.out.println("Total wages for a month of an employee :" +hrs*WagePerHr);
+        }
     }
-}
